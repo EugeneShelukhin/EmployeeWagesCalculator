64c9e14815adf1d28d6fb1d260dd83a82512d710
Eugene <e.sheluhin@b2btrade.ru>
2020-02-28 11:14:39 +0300

    test

--- Tests/TestBase.cs
+++ Tests/TestBase.cs
@@ -19,26 +19,33 @@ namespace Tests
             repository = new WorkersRepository(dataContext, idCounter, subordinatesCache);
         }
 
+        public long johnId;
+        public long janeId;
+        public long judiId;
+        public long jamesId;
+        public long piterId;
+
 
         public void LoadWorkers()
         {
-            var johnId = repository.AddManager(null, "John Doe", DateTime.ParseExact("2010-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
-            var janeId = repository.AddSales(johnId, "Jane Doe", DateTime.ParseExact("2011-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
-            var judiId = repository.AddSales(janeId, "Judy Doe", DateTime.ParseExact("2012-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
-            var jamesId = repository.AddManager(judiId, "James Doe", DateTime.ParseExact("2013-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
+            johnId = repository.AddManager(null, "John Doe", DateTime.ParseExact("2010-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
+            janeId = repository.AddSales(johnId, "Jane Doe", DateTime.ParseExact("2011-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
+            judiId = repository.AddSales(janeId, "Judy Doe", DateTime.ParseExact("2012-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
+            jamesId = repository.AddManager(judiId, "James Doe", DateTime.ParseExact("2013-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
+            piterId = repository.AddEmployee(jamesId, "Piter Peterson", DateTime.ParseExact("2015-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
             repository.AddEmployee(jamesId, "Ivan Ivanov", DateTime.ParseExact("2014-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
-            repository.AddEmployee(jamesId, "Piter Peterson", DateTime.ParseExact("2015-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
             repository.AddEmployee(null, "mr. Independent Worker", DateTime.ParseExact("2016-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
         }
 
         public void LoadManyWorkers()
         {
-            var johnId = repository.AddManager(null, "John Doe", DateTime.ParseExact("2010-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
-            var janeId = repository.AddSales(johnId, "Jane Doe", DateTime.ParseExact("2011-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
-            var judiId = repository.AddSales(janeId, "Judy Doe", DateTime.ParseExact("2012-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
-            var jamesId = repository.AddManager(judiId, "James Doe", DateTime.ParseExact("2013-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
+            johnId = repository.AddManager(null, "John Doe", DateTime.ParseExact("2010-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
+            janeId = repository.AddSales(johnId, "Jane Doe", DateTime.ParseExact("2011-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
+            judiId = repository.AddSales(janeId, "Judy Doe", DateTime.ParseExact("2012-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
+            jamesId = repository.AddManager(judiId, "James Doe", DateTime.ParseExact("2013-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
 
-            for (var i = 0; i < 1000; i++) {
+            for (var i = 0; i < 1000; i++)
+            {
                 repository.AddManager(null, $"John Doe {i}", DateTime.ParseExact("2010-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
                 repository.AddSales(johnId, $"Jane Doe {i}", DateTime.ParseExact("2011-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
                 repository.AddSales(janeId, $"Judy Doe {i}", DateTime.ParseExact("2012-12-31", "yyyy-MM-dd", CultureInfo.InvariantCulture));
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -12,14 +12,14 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
+            LoadWorkers();
         }
 
-        //подсчитываем зарплаты каждого и сравниваем со значением общей зарплаты
+        //check that total sum equals to sum of wages calculated one by one for each worker
         [Test]
-        public void Test1()
+        public void GetTotalWagesSum()
         {
             //arrange
-            LoadWorkers();
             var date = DateTime.ParseExact("2020-02-24", "yyyy-MM-dd", CultureInfo.InvariantCulture);
             var workers = repository.GetAll();
             //act
@@ -32,19 +32,39 @@ namespace Tests
             Assert.That(Math.Round(oneByOneSum, 2),  Is.EqualTo(825155.03));
         }
 
-
-        public void GetWagesForEmployee() { 
-        
+        [Test]
+        public void GetWagesForEmployee() {
+            //arrange
+            var date = DateTime.ParseExact("2020-02-24", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            var wageCalculator = new WagesCalculator(base.repository, new CustomCache<decimal>());
+            //act
+            var result = wageCalculator.CalculateWorkersWages(date, base.piterId);
+            //assert
+            Assert.That(result, Is.EqualTo(112000));
         }
 
+        [Test]
         public void GetWagesForManager()
         {
-
+            //arrange
+            var date = DateTime.ParseExact("2020-02-24", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            var wageCalculator = new WagesCalculator(base.repository, new CustomCache<decimal>());
+            //act
+            var result = wageCalculator.CalculateWorkersWages(date, base.jamesId);
+            //assert
+            Assert.That(result, Is.EqualTo(131135));
         }
 
+        [Test]
         public void GetWagesForSales()
         {
-
+            //arrange
+            var date = DateTime.ParseExact("2020-02-24", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            var wageCalculator = new WagesCalculator(base.repository, new CustomCache<decimal>());
+            //act
+            var result = wageCalculator.CalculateWorkersWages(date, base.judiId);
+            //assert
+            Assert.That(result, Is.EqualTo(108074.40500000M));
         }
     }
 }
